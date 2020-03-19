754d6096bdc7dfca39483c24caf79d6e4f527151
danbahrami <dan.bahrami@googlemail.com>
2020-03-19 21:01:50 +0000

    Customize splash page

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,6 @@
-<script>
-	export let name;
-</script>
-
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>The Indoor Society</h1>
+	<p>Coming soon!</p>
 </main>
 
 <style>
