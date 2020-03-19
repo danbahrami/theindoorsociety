0ba58ee7e248fb5b9557c2cb45a8484c7b5b0f42
danbahrami <dan.bahrami@googlemail.com>
2020-03-19 20:59:36 +0000

    Serve svelte app through express

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
 app.listen(PORT, () => console.log(`listening on port ${PORT}!`))
+
+// Serve our static files
+app.use(express.static(__dirname + '/../public'));
